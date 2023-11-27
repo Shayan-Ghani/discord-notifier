bdfc063bef9601fa34ae6bd1fd0f0fbb4e3c6bf6
shayan ghani <shayanghani1384@gmail.com>
2023-11-27 12:37:56 +0330

    added environment variable config

--- Dockerfile
+++ Dockerfile
@@ -2,11 +2,13 @@ FROM python:3.10.4-slim-buster
 
 LABEL maintainer=shayanghani1384@gmail.com
 
+ENV TOKEN HOOK ADMIN_ID USER_ID
+
 WORKDIR /opt/app
 
 COPY ./requirements.txt .
 
-RUN pip install -r requirements.txt
+RUN pip install -r requirements.txt --no-cache-dir
 
 COPY . . 
 
--- discord-notifier.py
+++ discord-notifier.py
@@ -1,18 +1,24 @@
 from discord import Intents, Client
 from discord_webhook import DiscordWebhook
 
+from os import environ
 
 def notify():
 
+    TOKEN = environ.get("TOKEN" , None)
+    HOOK = environ.get("HOOK" , None)
+    ADMIN_ID = environ.get("ADMIN_ID", None)
+    USER_ID = environ.get("USER_ID", None)
+
     intents = Intents.default()
     intents.messages = True
-
+   
     client = Client(intents=intents)
 
-    webhook_url = "your discord webhook"
+    webhook_url = HOOK
 
-    admin_id = "some_id"
-    user_id = "some_other_id"
+    admin_id = ADMIN_ID
+    user_id = USER_ID
 
     @client.event
     async def on_ready():
@@ -26,4 +32,4 @@ def notify():
         webhook = DiscordWebhook(url=webhook_url, content=f"<@{admin_id}> <@{user_id}> A New Alert has been triggered")
         response = webhook.execute()
 
-    client.run('your bot token')
+    client.run(TOKEN)
